617aa063580ca97a6e43b432a3750019bdd9a6e9
avcialper <avcialper12@gmail.com>
2024-07-22 23:14:15 +0300

    boxed and unboxed description update

--- BoxedUnboxed/src/Main.kt
+++ BoxedUnboxed/src/Main.kt
@@ -2,8 +2,12 @@ fun main() {
 
     /**
      *      Bütün boxed değerlerde aynı sonuçları alacağımızı düşünebiliriz, ancak burada ince bir ayrıntı var. Eğer
-     *      unboxed değişkenin değeri, byte değer aralığında yani -128 ile 127 arasında ise, bu değişkenler referans
+     *      boxed değişkenin değeri, byte değer aralığında yani -128 ile 127 arasında ise, bu değişkenler referans
      *      olarak aynı nesneyi işaret ederler. Bu durum JVM tarafında yapılan özel optimizasyonlardan kaynaklanır.
+     *
+     *      Burada değişkenler byte, short, int ve long değerler olabilirler, float ve double için bu durum geçerli
+     *      değildir. Ayrıca karşılaştırma sırasında iki değişkenin de aynı tipte olması gerekir, aynı olmazlarsa IDE
+     *      hata gösterecektir.
      */
 
     val numberOne: Int = 120
